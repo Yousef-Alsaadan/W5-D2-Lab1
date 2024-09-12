7d73480f79797d5c184b30721c760798e5b6dd80
Yousef M <yousefmalsaadan@gmail.com>
2024-09-12 11:31:34 +0300

    W5-D2-Lab1

---
+++ main.js
@@ -0,0 +1,21 @@
+let text = "text";
+let text_main = document.getElementById("text_main");
+function changes_text() {
+    text_main.textContent = "My name is Yousef.."
+}
+function increases_size() {
+    text_main.style.fontSize = "5em";
+}
+function decreases_size() {
+    text_main.style.fontSize = "1em";
+}
+function changes_color() {
+    text_main.style.color = "green";
+}
+function background_color() {
+    text_main.style.background = "BLUE";
+}
+
+function addImg() {
+    
+}
